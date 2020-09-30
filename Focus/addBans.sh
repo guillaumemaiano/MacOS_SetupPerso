7bf9a67fc768b71e99df94e34f771bc1e74e4b5f
GEM <guillaume@maiano.fr>
2020-09-30 11:44:31 +0200

    Productivity bans

---
+++ Focus/addBans.sh
@@ -0,0 +1,21 @@
+echo "Saving older hosts file"
+cp /etc/hosts /etc/hosts.old
+echo "Banning Facebook"
+echo "IPv4"
+echo "127.0.0.1 facebook.com" >> /etc/hosts
+echo "IPv6"
+echo "::1 facebook.com" >> /etc/hosts
+echo "Banning Twitter"
+echo "IPv4"
+echo "127.0.0.1 twitter.com" >> /etc/hosts
+echo "IPv6"
+echo "::1 twitter.com" >> /etc/hosts
+echo "Banning News"
+echo "IPv4"
+echo "127.0.0.1 news.google.com" >> /etc/hosts
+echo "IPv6"
+echo "::1 news.google.com" >> /etc/hosts
+echo "Reloading DNS cache"
+dscacheutil -flushcache
+killall -HUP mDNSResponder
+echo "Productivity increased"
