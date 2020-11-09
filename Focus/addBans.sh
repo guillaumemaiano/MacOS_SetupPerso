893954f8627308717f4e1d4a409a1c6ba3699e7f
Guillaume MAIANO <guillaume@maiano.fr>
2020-11-09 12:43:13 +0100

    Improve bans

--- Focus/addBans.sh
+++ Focus/addBans.sh
@@ -3,8 +3,10 @@ cp /etc/hosts /etc/hosts.old
 echo "Banning Facebook"
 echo "IPv4"
 echo "127.0.0.1 facebook.com" >> /etc/hosts
+echo "127.0.0.1 www.facebook.com" >> /etc/hosts
 echo "IPv6"
 echo "::1 facebook.com" >> /etc/hosts
+echo "::1 www.facebook.com" >> /etc/hosts
 echo "Banning Twitter"
 echo "IPv4"
 echo "127.0.0.1 twitter.com" >> /etc/hosts
